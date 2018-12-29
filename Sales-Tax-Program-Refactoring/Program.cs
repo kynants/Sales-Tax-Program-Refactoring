3084a7aebea9ecc9b7e1526808604ec5e226b8ca
Kynan Stevenson <kynants@gmail.com>
2018-12-29 17:10:00 -0500

    Initial Commit

---
+++ Sales-Tax-Program-Refactoring/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sales_Tax_Program_Refactoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt User Input
+            Console.Write("Enter the amount of a purchase: ");
+            double purchase = Convert.ToDouble(Console.ReadLine());
+
+            // Initialized Tax Variables
+            double stateTax = 0.04,
+                   countyTax = 0.02;
+
+            // Final Result Output
+            Console.WriteLine("Amount of the purchase: $" + purchase);
+            Console.WriteLine("State sales tax: $" + stateTax * purchase);
+            Console.WriteLine("County sales tax: $" + countyTax * purchase);
+            Console.WriteLine("Total sales tax: $" + 
+                              (stateTax + countyTax) * purchase);
+            Console.WriteLine("Total of the sale: $" + 
+                              (purchase += ((stateTax + countyTax) * purchase)));
+        }
+    }
+}
